ff1765b213dbd3b8e5aab6e1da62941a7483dc9b
ahmed <hamzaa667788@gmail.com>
2024-03-30 21:13:47 -0400

    initialized clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-
+import './App.css'
 function App() {
 
   return (
     <>
- 
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <div className="absolute top-5 right-2 text-sky-400 text-xl text-center " >
+        <SignedOut>
+          <SignInButton mode="modal"/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
       <h1 className="text-3xl font-bold">Welcome to SimStock-Exchange!</h1>
       <p className="text-xl text-blue-700 font-bold">coming soon...</p>
     </>
