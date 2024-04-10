fca01145f9903a39e2cd6eee08077c0a0fc0973e
Adefela Fakorode <adefakorode@gmail.com>
2024-04-09 20:11:06 -0400

    finished ess. for Landing Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,6 @@ function App() {
         </SignedIn>
       </div>
 */}
-
       <LandingPage />
     </>
   )
--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -1,16 +1,31 @@
+import SimStockLogo from "../assets/SimStockLogo.png";
 function LandingPage() {
   return (
     <section className="min-h-screen bg-white">
-        <div className="container mx-auto py-[160px] flex flex-row-reverse justify-center items-center h-[800px] bg-red-400">
-            <div className=" flex justify-center items-center">
-                
-            </div>
-            <div>
-                
-            </div>
+      <div className="container mx-auto flex flex-col items-center justify-center lg:flex-row-reverse mt-[100px]">
+        <div className="xl:ml-[100px] lg:ml-8">
+          {/*Image*/}
+          <img src={SimStockLogo} alt="PP" className="w-[400px] xl:w-[600px]" />
         </div>
+
+        <div className="mt-[30px]">
+          {/*Title*/}
+          <h1 className="text-5xl font-semibold xl:text-[80px] ">
+            SimStock Exchange
+          </h1>
+          {/*Text*/}
+          <p className="mt-[10px] xl:text-[20px] ">
+            All-in-one Platform that helps investors practice their trading
+            skills.
+          </p>
+          {/*Button*/}
+          <button className=" xl:mt-[20px] xl:text-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold xl:py-5 xl:px-40 rounded">
+          Open an account →
+          </button>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
