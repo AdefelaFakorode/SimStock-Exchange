8a17e20702b8b3e5a086c4a2778b56cd6ce5cd33
ahmed <hamzaa667788@gmail.com>
2024-04-16 19:48:01 -0400

    sat up trading page

--- client/src/views/TradingPage.jsx
+++ client/src/views/TradingPage.jsx
@@ -1,13 +1,19 @@
 import LPNavBar from '../components/LSI_NavBar/LPNavBar';
 import Trending_Comp from '../components/trading_page/Trending_Comp';
-
+import Graph_Comp from '../components/trading_page/Graph_Comp';
 
 function TradingPage() {
     return (
         <>
             <LPNavBar />
-            <h2 className="text-lg md:text-xl lg:text-2xl text-[#8a1e85] font-bold  p-2 text-center">Trading page</h2>
-            <Trending_Comp />
+            <div className="flex">
+                <div className="w-1/2">
+                <Trending_Comp />
+                </div>
+                <div className="w-1/2">
+                <Graph_Comp />
+                </div>
+            </div>
         </>
 
     )
