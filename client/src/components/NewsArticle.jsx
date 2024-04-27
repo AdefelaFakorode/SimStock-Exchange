93c6f9757f37fb70655382d4e7f4e6139d6a839a
jawadrada <jawadrada18@gmail.com>
2024-04-27 02:26:21 -0400

    Fixed NewsFeed up
    Added a refresh rate, button to load more articles, and a fixed styling

--- client/src/components/NewsArticle.jsx
+++ client/src/components/NewsArticle.jsx
@@ -8,16 +8,16 @@ function NewsArticle(props) {
 
     return (
         <>
-            <div className="container p-1 my-4">
-                <h2 className="text-lg md:text-xl lg:text-2xl text-[#8a1e85] font-bold underline p-2 text-center">{props.title}</h2>
-                <div className="p-2 text-xs md:text-sm lg:text-lg flex justify-between text-[#e4d5f6]">
+            <div className="container p-5 my-10 bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+                <h2 className="text-lg md:text-xl lg:text-2xl text-black font-bold underline p-2 text-center">{props.title}</h2>
+                <div className="p-2 text-xs md:text-sm lg:text-lg flex justify-between text-black">
                     <h6 className="">Author: {props.author}</h6>
                     <h6 className="">Date Published: {date}</h6>
                 </div>
-                <div className="text-[#e4d5f6]">
+                <div className="text-black">
                     <img className="p-2" src={props.image} alt="" />
                     <p className="p-2 text-sm md:text-base lg:text-xl">{props.description}</p>
-                    <a className="text-sm md:text-base lg:text-lg text-blue-500 hover:text-blue-700 p-2 my-2" href={props.url}>Click Here to Read the Full Article</a>
+                    <a className="text-sm md:text-base lg:text-lg text-blue-500 hover:text-blue-700 p-2 my-2" href={props.url} target="blank">Click Here to Read the Full Article</a>
                 </div>
             </div>
         </>
