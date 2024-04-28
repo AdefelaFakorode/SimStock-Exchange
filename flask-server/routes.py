d43cd11e3b3edab5f96978c97237141050179901
ddamme05 <ddamme05@gmail.com>
2024-04-28 00:05:08 -0400

    Added routes for yfinance fetching.

---
+++ flask-server/routes.py
@@ -0,0 +1,70 @@
+import yfinance as yf
+
+
+from flask import Flask, jsonify, request
+
+
+def fetch_company_details(ticker):
+
+    company = yf.Ticker(ticker)
+
+    info = company.info
+
+    return jsonify(info)
+
+
+def fetch_historical_data(ticker):
+
+    period = request.args.get("period", "1y")
+
+    interval = request.args.get("interval", "1d")
+
+    ticker = yf.Ticker(ticker)
+
+    try:
+
+        data = ticker.history(period=period, interval=interval)
+
+        if data.empty:
+
+            return jsonify({"error": "No historical data found."}), 404
+
+        data.reset_index(inplace=True)
+
+        data["Date"] = data["Date"].dt.strftime("%Y-%m-%d")
+        data_dict = data.to_dict(orient="records")
+
+        return jsonify(data_dict), 200
+
+    except Exception as e:
+
+        return jsonify({"error": str(e)}), 500
+
+
+def fetch_latest_quote(ticker):
+
+    ticker = yf.Ticker(ticker)
+
+    try:
+
+        data = ticker.history(period="1d")
+
+        if data.empty:
+
+            return jsonify({"error": "No latest quote data found."}), 404
+
+        last_valid_index = data["Close"].last_valid_index()
+
+        if last_valid_index is not None:
+
+            latest_data = data.loc[last_valid_index].to_dict()
+
+            latest_data["Date"] = last_valid_index.strftime("%Y-%m-%d")
+
+            return jsonify(latest_data), 200
+
+        return jsonify({"error": "No valid data found for the latest quote."}), 404
+
+    except Exception as e:
+
+        return jsonify({"error": str(e)}), 500
