bc38681a64e769712c2b81388d35bffecdf3b5de
jawadrada <jawadrada18@gmail.com>
2024-04-29 21:11:08 -0400

    Updated Footer and NavBar

--- client/src/components/LSI_NavBar/LSI_Footer.jsx
+++ client/src/components/LSI_NavBar/LSI_Footer.jsx
@@ -1,14 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebookF, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 function LSI_Footer() {
   return (
-    <div className="w-full h-[120px] bg-background flex justify-end items-center">
-      <div className=" px-5 text-text">
-        <p>
-          Copyright ©2024 SimStock-Exchange
-          <br />
-        </p>
+    <div className="w-full bg-background border-t-2 border-t-white text-white px-10">
+      <div className="max-w-7xl flex flex-col sm:flex-row py-4 mx-auto justify-between items-center">
+        <div className="text-center">
+          <div>
+            Copyright ©2024 <strong>SimStock-Exchange</strong>.
+          </div>
+        </div>
+        <div className="text-center text-xl text-white mb-2">
+          <a href="#" className="w-10 h-10 rounded-full bg-white hover:bg-gray-400 mx-1 inline-block pt-1">
+            <FontAwesomeIcon icon={faTwitter} className="text-blue-500" />
+          </a>
+          <a href="#" className="w-10 h-10 rounded-full bg-white hover:bg-gray-400 mx-1 inline-block pt-1">
+            <FontAwesomeIcon icon={faInstagram} className="text-pink-500" />
+          </a>
+          <a href="#" className="w-10 h-10 rounded-full bg-white hover:bg-gray-400 mx-1 inline-block pt-1">
+            <FontAwesomeIcon icon={faFacebookF} className="text-blue-600" />
+          </a>
+          <a href="#" className="w-10 h-10 rounded-full bg-white hover:bg-gray-400 mx-1 inline-block pt-1">
+            <FontAwesomeIcon icon={faLinkedin} className="text-blue-700" />
+          </a>
+        </div>
       </div>
     </div>
   );
 }
 
 export default LSI_Footer;
+
+
+
