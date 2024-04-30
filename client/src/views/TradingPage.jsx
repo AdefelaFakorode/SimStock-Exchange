1166f8e9c30fe9b6237cbffe589f87f464318b9d
ahmed <hamzaa667788@gmail.com>
2024-04-30 19:36:52 -0400

    overview and details comp

--- client/src/views/TradingPage.jsx
+++ client/src/views/TradingPage.jsx
@@ -3,9 +3,21 @@ import Trending_Comp from '../components/trading_page/Trending_Comp';
 import Graph_Comp from '../components/trading_page/Graph_Comp';
 import Footer from '../components/LSI_NavBar/LSI_Footer.jsx';
 import Search from '../components/Search.jsx';
+import Details from '../components/Details';
+import Overview from '../components/Overview';
 
 function TradingPage() {
-    
+    //mock company details replace with real api calls
+    const mockCompanyDetails={
+      name: "Apple",
+      country: "US",
+      currency: "USD",
+      exchange: "NASDAQ/NMS (GLOBAL MARKET)",
+      ipo: "1980-12-12",
+      marketCapitalization: "1415993",
+      ticker: "AAPL",
+      finnhubIndustry: "Technology",
+    }
     return (<div>
       <LPNavBar/>
       <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
@@ -19,10 +31,16 @@ function TradingPage() {
           <div>Chart</div>
         </div>
         <div className='bg-neutral-400'>
-          <div>Overview</div>
+          <Overview 
+            symbol={mockCompanyDetails.ticker} 
+            price={300}
+            change={30}
+            changePercent={10.0}
+            currency={"USD"}
+            />
         </div>
         <div className="row-span-2 xl:row-span-3 bg-neutral-400">
-          <div>Details</div>
+          <Details details={mockCompanyDetails} />
         </div>
       </div>
 <Footer />
