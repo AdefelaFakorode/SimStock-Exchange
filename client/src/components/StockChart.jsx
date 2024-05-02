c92ed9b33122e1b5b2bc55f1a3d779efdfcb9b08
ahmed <hamzaa667788@gmail.com>
2024-05-02 13:31:09 -0400

    using useState to change ticker

--- client/src/components/StockChart.jsx
+++ client/src/components/StockChart.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import axios from 'axios';
 
-function StockChart() {
+function StockChart({ticker}) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try{
-        const reponse = await axios.get('http://127.0.0.1:5000/history/msft');
+        const reponse = await axios.get(`http://127.0.0.1:5000/history/${ticker}`);
         setData(reponse.data);
       } catch(error){
         console.error('Failed to fetch data', error)
