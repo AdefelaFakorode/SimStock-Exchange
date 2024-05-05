6466cd37721db42b78f625ce5e57aeefc324bdc5
Adefela Fakorode <adefakorode@gmail.com>
2024-05-05 15:27:19 -0500

    working on landing page still

---
+++ client/src/components/Border.jsx
@@ -0,0 +1,12 @@
+
+function Border() {
+  return (
+    <div>
+      <div>
+<hr className="border-0 h-[.1px] bg-[#D9D9D9] w-[1500px] opacity-50" /> {/* Line for divider */}
+</div>
+    </div>
+  )
+}
+
+export default Border
