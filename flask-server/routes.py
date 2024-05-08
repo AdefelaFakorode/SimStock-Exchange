1b2bdf5a9d1b9897527daa791bf1ccacb46c8a9c
ddamme05 <ddamme05@gmail.com>
2024-05-07 21:54:02 -0400

    Queried Clerk for user info through HTTP requests.

--- flask-server/routes.py
+++ flask-server/routes.py
@@ -1,15 +1,26 @@
 import yfinance as yf
+import os
+import requests
+from flask import jsonify
 
+CLERK_API_URL = "https://api.clerk.dev/v1"
+CLERK_API_KEY = os.getenv("CLERK_API_KEY")
 
-from flask import Flask, jsonify, request
-
+def fetch_clerk_users():
+    """Fetch all user details from Clerk."""
+    headers = {
+        "Authorization": f"Bearer {CLERK_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get(f"{CLERK_API_URL}/users", headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 def fetch_company_details(ticker):
-
     company = yf.Ticker(ticker)
-
     info = company.info
-
     return jsonify(info)
 
 
@@ -24,34 +35,20 @@ def fetch_historical_data(ticker, period="1y", interval="1d"):
         data_dict = data.to_dict(orient="records")
         return jsonify(data_dict), 200
     except Exception as e:
-        print(f"Error fetching data for {ticker} with interval {interval} and period {period}: {e}")
         return jsonify({"error": str(e)}), 500
 
 
 def fetch_latest_quote(ticker):
-
     ticker = yf.Ticker(ticker)
-
     try:
-
         data = ticker.history(period="1d")
-
         if data.empty:
-
             return jsonify({"error": "No latest quote data found."}), 404
-
         last_valid_index = data["Close"].last_valid_index()
-
         if last_valid_index is not None:
-
             latest_data = data.loc[last_valid_index].to_dict()
-
             latest_data["Date"] = last_valid_index.strftime("%Y-%m-%d")
-
             return jsonify(latest_data), 200
-
         return jsonify({"error": "No valid data found for the latest quote."}), 404
-
     except Exception as e:
-
         return jsonify({"error": str(e)}), 500
