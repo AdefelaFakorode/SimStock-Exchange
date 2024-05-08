c7e09ac113eafce6d07bd4bddb17e406bf1aba2e
ddamme05 <ddamme05@gmail.com>
2024-05-08 02:04:19 -0400

    Refactored CORS and simplified it.

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,23 +5,29 @@ import logging
 import requests
 import os
 from dotenv import load_dotenv
-from models import db, User, Transaction, UserStocks, NewsFeed
-from routes import fetch_company_details, fetch_historical_data, fetch_latest_quote, fetch_clerk_users
 
 load_dotenv('.env.local')
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+CORS(app)
+
 logging.basicConfig(level=logging.DEBUG)
+
+from models import db, User, Transaction, UserStocks, NewsFeed
 db.init_app(app)
 
-@app.route('/api/')
+from routes import fetch_company_details, fetch_historical_data, fetch_latest_quote, fetch_clerk_users
+
+@app.route('/')
 def hello_world():
     return 'Hello, from Flask!'
 
-@app.route('/api/users')
+@app.route('/users')
 def get_users():
     users_details = fetch_clerk_users()
     if users_details:
@@ -29,11 +35,15 @@ def get_users():
     else:
         return jsonify({"error": "Failed to fetch user details"}), 404
 
-@app.route('/api/company/<ticker>')
-def company_details_view(ticker):
-    return company_details(ticker)
+@app.route('/company/<ticker>')
+def company_details(ticker):
+    try:
+        return fetch_company_details(ticker)
+    except Exception as e:
+        logging.error(f"Failed to fetch company details for {ticker}: {str(e)}")
+        return jsonify({'error': 'Failed to fetch company details', 'details': str(e)}), 500
 
-@app.route('/api/search', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def proxy_search():
     query = request.args.get('q')
     if not query:
@@ -41,17 +51,25 @@ def proxy_search():
     response = requests.get(f'https://query1.finance.yahoo.com/v1/finance/search?q={query}')
     return jsonify(response.json())
 
-@app.route('/api/history/<ticker>')
-def history_view(ticker):
+@app.route('/history/<ticker>')
+def history(ticker):
     period = request.args.get('period', '1y')
     interval = request.args.get('interval', '1d')
-    return fetch_historical_data(ticker, period, interval)
+    try:
+        return fetch_historical_data(ticker, period, interval)
+    except Exception as e:
+        print(f"Failed to fetch historical data for {ticker}: {str(e)}")
+        return jsonify({'error': 'Failed to fetch historical data', 'details': str(e)}), 500
 
-@app.route('/api/quote/<ticker>')
-def quote_view(ticker):
-    return fetch_latest_quote(ticker)
+@app.route('/quote/<ticker>')
+def quote(ticker):
+    try:
+        return fetch_latest_quote(ticker)
+    except Exception as e:
+        logging.error(f"Failed to fetch the latest quote for {ticker}: {str(e)}")
+        return jsonify({'error': 'Failed to fetch the latest quote', 'details': str(e)}), 500
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
