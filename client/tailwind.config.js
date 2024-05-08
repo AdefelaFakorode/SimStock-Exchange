9c5727b924792ed465704dd58d96b7ba17c61699
ahmed <hamzaa667788@gmail.com>
2024-05-08 00:58:21 -0400

    styling stock chart

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,9 @@ export default {
         'text': '#878787',
         'background': '#0C0D0E',
         'buttonColor': '#E0DACD',
-        'hoverButtonColor': '#8F8B83'
+        'hoverButtonColor': '#8F8B83',
+        'dark-blue': '#243c5a', 
+        'light-blue': '#4a8cbb' 
       },
 
       fontFamily: {
